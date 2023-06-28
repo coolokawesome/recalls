0123f6e9a840cd45594f7fa63c1ac6807ea4787d
rickyjoe11 <syrus.samson99@gmail.com>
2023-06-27 22:53:51 -0400

    Updated Header theme

--- canada-recalls/src/HomepageHeader.jsx
+++ canada-recalls/src/HomepageHeader.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function HomepageHeader() {
   return (
-    <div className='container homepage-header-container'>
+    <div className=' homepage-header-container'>
         <div className='homepage-header'>
             <h1>Canada Recalls</h1>
         </div>
