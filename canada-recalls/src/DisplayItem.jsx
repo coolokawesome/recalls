ec6d2f11ba419693164610193a762acfabf20137
rickyjoe11 <syrus.samson99@gmail.com>
2023-06-27 22:54:41 -0400

    updated display functionality

--- canada-recalls/src/DisplayItem.jsx
+++ canada-recalls/src/DisplayItem.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { ModalInfo, ModalState, Json } from './Atoms'
+import { atom, useAtom } from 'jotai'
 
 function DisplayItem(props) {
+  const [modalInfo, setModalInfo] = useAtom(ModalInfo)
+  const [modalState, setModalState] = useAtom(ModalState)
+  const [json, setJson] = useAtom(Json)
+  //set the new modal information 
+
+
+  const handleSeeMore = (e) => {
+    const searchableID = e.target.id
+    const handleFind = json.find(element => element.NID == searchableID.toString())
+    console.log('handleFind (id): ' + handleFind, 'the Elements ID: ' + searchableID.toString())
+    setModalInfo(
+      handleFind
+    );
+    setModalState(true)
+  }
+
   return (
-    <div className='col-12 col-md-6 col-lg-4 col-xl-3 p-3 display-item-outer' key={props.NID} id={`item-${props.NID}`}>
+    <div className='col-12 display-item-outer' key={props.NID} id={`item-${props.NID}`}>
         <div className='display-item-inner'>
           <div className='top-card row'>
             <h3 className='col-12'>{props.Title}</h3>
@@ -13,6 +31,7 @@ function DisplayItem(props) {
             <h5>Last Updated: {props.updated}</h5>
           </div>
       </div>
+      <button id={props.NID} onClick={handleSeeMore}>See More</button>
     </div>
       
      
