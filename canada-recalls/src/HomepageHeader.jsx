7bfc37a977ee1eace5ca3f98b1177d7ac748a6cf
rickyjoe11 <syrus.samson99@gmail.com>
2023-07-05 09:26:40 -0400

    updatd homepage header

--- canada-recalls/src/HomepageHeader.jsx
+++ canada-recalls/src/HomepageHeader.jsx
@@ -5,11 +5,13 @@ function HomepageHeader() {
     <div className='homepage-header-container'>
         <div className='homepage-header'>
             <div className='homepage-header-inner container d-flex'>
-            <div><h1>Canadian Recalls</h1></div>
+              <div>
+                <h1>Canadian Recalls</h1>
+              </div>
             <div>
               <a style={{
                 color: 'white',
-                
+    
               }} target='_blank' href={'https://github.com/coolokawesome/recalls'}> Repo </a>
             </div>
             </div>
