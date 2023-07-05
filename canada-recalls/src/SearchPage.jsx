16f3013a2cbe7ddbd36fa3a867b6fe42444c687a
rickyjoe11 <syrus.samson99@gmail.com>
2023-07-05 09:26:30 -0400

    updated search

---
+++ canada-recalls/src/SearchPage.jsx
@@ -0,0 +1,15 @@
+import ItemModal from './ItemModal';
+import Search from './Search';
+
+function SearchPage() {
+  return (
+    <>
+
+
+                <Search />
+                <ItemModal  />
+  </>
+  )
+}
+
+export default SearchPage
